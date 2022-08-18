496184df0af2c8141b60db79cd0ae9cf8b76d9e1
PolinaG <PolinaG@gmail.com>
2022-08-18 19:31:04 +0300

    Add Example005 If-Else

---
+++ Example005_IfElse/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите имя пользователя ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "polina")
+{
+    Console.WriteLine("Ура, это же Polina!");    
+}
+
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
